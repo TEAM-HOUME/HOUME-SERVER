b492b37b8a312b59b4cfb473c7b736a7a411a03d
gdbs1107 <gdbs1107@naver.com>
2025-06-20 09:05:54 +0900

    init: 프로젝트 설정

---
+++ src/main/java/or/sopt/houme/HoumeApplication.java
@@ -0,0 +1,13 @@
+package or.sopt.houme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoumeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/or/sopt/houme/HoumeApplicationTests.java
@@ -0,0 +1,13 @@
+package or.sopt.houme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
