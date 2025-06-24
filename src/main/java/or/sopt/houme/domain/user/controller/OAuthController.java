5f3d794b0472da7aed8dd332495af4305d932e80
gdbs1107 <gdbs1107@naver.com>
2025-06-24 17:57:55 +0900

    feat:#3 리다이렉트 주소 클라이언트로 변환

--- src/main/java/or/sopt/houme/domain/user/controller/OAuthController.java
+++ src/main/java/or/sopt/houme/domain/user/controller/OAuthController.java
@@ -21,7 +21,8 @@ public class OAuthController {
     @GetMapping("/oauth/kakao")
     @Operation(summary = "카카오 소셜로그인 API",
             description = "카카오로 로그인 요청을 전송합니다. <br><br>" +
-                    "카카오 인증서버로 요청을 보내고 그 후에 **백엔드의 주소**로 리다이렉트 됩니다.")
+                    "카카오 인증서버로 요청을 보내고 그 후에 **localhost:3000**로 리다이렉트 됩니다. <br><br>" +
+                    "리다이렉트 주소에 포함되어 있는 인가코드를 밑의 API의 파라미터에 넣어주세요")
     public void kakaoOAuthCallback(HttpServletResponse response) throws IOException {
 
         String redirectAddress = oAuthService.requestRedirect();
@@ -31,8 +32,7 @@ public class OAuthController {
 
     @Operation(summary = "카카오 인증서버 토큰 검증 API",
     description = "리다이렉트에서 AccessCode를 가지고 서버로 돌아오기 위한 엔드포인트입니다 <br><br><br>" +
-            "해당 코드를 이용해서 사용자 정보를 파싱하고 **액세스 토큰과 리프레시 토큰을 모두 쿠키에 담아** 반환합니다 <br><br>" +
-            "클라이언트 단에서 다시 한 번 서버로 요청을 주시면 쿠키에 담겨있는 액세스토큰을 헤더로 옮겨 드립니다")
+            "해당 코드를 이용해서 사용자 정보를 파싱하고 **액세스 토큰는 헤더에, 리프레시 토큰은 쿠키에 담아** 반환합니다")
     @GetMapping("/oauth/kakao/callback")
     public void kakaoLogin(@RequestParam("code") String accessCode, HttpServletResponse response) {
         oAuthService.kakaoLogin(accessCode,response);
