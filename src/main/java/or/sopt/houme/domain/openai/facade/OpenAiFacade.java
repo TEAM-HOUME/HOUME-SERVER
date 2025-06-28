e9fda999142d451741924c7f7c295370602d6c89
gdbs1107 <gdbs1107@naver.com>
2025-06-28 18:36:41 +0900

    feat:#10 facade 로직 구현

--- src/main/java/or/sopt/houme/domain/openai/facade/OpenAiFacade.java
+++ src/main/java/or/sopt/houme/domain/openai/facade/OpenAiFacade.java
@@ -2,6 +2,7 @@ package or.sopt.houme.domain.openai.facade;
 
 import lombok.RequiredArgsConstructor;
 import or.sopt.houme.domain.openai.service.OpenAiService;
+import or.sopt.houme.domain.prompt.dto.PromptRequestDTO;
 import or.sopt.houme.domain.prompt.util.PromptService;
 import org.springframework.stereotype.Component;
 
@@ -12,5 +13,26 @@ public class OpenAiFacade {
     private final OpenAiService openAiService;
     private final PromptService promptService;
 
+    /**
+     * 1. DTO 를 통해서 이미지 생성에 필요한 데이터를 받습니다
+     * 2. 그러면 해당 데이터를 파싱하여 프롬프팅하고
+     * 3. 그 결과를 Chatgpt-image-1 모델에 넣어 이미지를 반환합니다
+     *
+     * 결론적으로 채륜님이 만드는 API 로직 중 해당 메서드만 호출하면
+     *
+     * 0. 프롬프트 제작
+     * 1. 생성형 이미지 생성하고 반환
+     * 2. S3에 이미지 저장
+     * 4. 데이터베이스에 이미지 저장
+     *
+     * 까지의 로직을 할 수 있도록 구현하였습니다
+     * */
+    public byte[] makeImage(PromptRequestDTO promptRequestDTO){
 
+        String prompt = promptService.makePrompt(promptRequestDTO);
+
+        // 데이터베이스 이미지 저장
+
+        return openAiService.createImage(prompt);
+    }
 }
