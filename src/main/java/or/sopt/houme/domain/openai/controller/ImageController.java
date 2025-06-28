06acc9cc5c3bb8c30addbbe498d831cc368f5e24
gdbs1107 <gdbs1107@naver.com>
2025-06-28 14:48:56 +0900

    feat:#10 chatgpt-image-1 API 연동
    # Conflicts:
    #	src/main/resources/application-local.yml

---
+++ src/main/java/or/sopt/houme/domain/openai/controller/ImageController.java
@@ -0,0 +1,29 @@
+package or.sopt.houme.domain.openai.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import or.sopt.houme.domain.openai.service.ImageService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/image")
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @Operation(summary = "chatgpt-image-1 을 활용한 이미지 저장 API")
+    @GetMapping(value = "/generate", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> generate(@RequestParam String prompt) {
+
+        byte[] image = imageService.createImage(prompt);
+
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(image);
+    }
+
+}
