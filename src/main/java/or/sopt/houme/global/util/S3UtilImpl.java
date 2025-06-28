0b9ea7c1730170ac36384fc685725659a37312f4
gdbs1107 <gdbs1107@naver.com>
2025-06-28 15:31:48 +0900

    feat:#10 S3 업로드 로직 비동기적으로 작동하도록 수정

--- src/main/java/or/sopt/houme/global/util/S3UtilImpl.java
+++ src/main/java/or/sopt/houme/global/util/S3UtilImpl.java
@@ -1,11 +1,13 @@
 package or.sopt.houme.global.util;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class S3UtilImpl implements S3Util {
 
     @Override
+    @Async
     public void upload(byte[] image, String dirname){
 
         throw new IllegalArgumentException("S3를 활용한 upload 메서드가 아직 구현되지 않았습니다");
