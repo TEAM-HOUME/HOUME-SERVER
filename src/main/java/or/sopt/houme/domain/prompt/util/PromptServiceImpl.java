959da1c716b20b6a79df81cf7c4a21fdd96cf24c
gdbs1107 <gdbs1107@naver.com>
2025-06-28 18:36:41 +0900

    feat:#10 facade 로직 구현

--- src/main/java/or/sopt/houme/domain/prompt/util/PromptServiceImpl.java
+++ src/main/java/or/sopt/houme/domain/prompt/util/PromptServiceImpl.java
@@ -2,6 +2,7 @@ package or.sopt.houme.domain.prompt.util;
 
 import lombok.RequiredArgsConstructor;
 import or.sopt.houme.domain.openai.service.OpenAiService;
+import or.sopt.houme.domain.prompt.dto.PromptRequestDTO;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,10 +12,11 @@ public class PromptServiceImpl implements PromptService {
     private final OpenAiService imageService;
 
     @Override
-    public void makePrompt() {
+    public String makePrompt(PromptRequestDTO requestDTO) {
 
         String result = "프롬프트 제작 계획이 확정되면 구현합니다";
 
-        imageService.createImage(result);
+        // 임시로 프롬프트를 넣어보았습니다. 원래는 requestDTO를 기반으로 프롬프트를 만들어서 이를 반환합니다 FIXME
+        return "웃는 남자의 이미지를 뽑아줘";
     }
 }
