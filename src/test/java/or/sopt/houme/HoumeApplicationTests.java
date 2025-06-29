1f9696cce2efc0a5196fc559a55651686e7d0c18
gdbs1107 <gdbs1107@naver.com>
2025-06-29 21:22:11 +0900

    feat:#13 테스트 체크

--- src/test/java/or/sopt/houme/HoumeApplicationTests.java
+++ src/test/java/or/sopt/houme/HoumeApplicationTests.java
@@ -1,12 +1,18 @@
 package or.sopt.houme;
 
+import jdk.jfr.Name;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
+@DisplayName("테스트 환경 Health Check")
 @SpringBootTest
 class HoumeApplicationTests {
 
     @Test
+    @DisplayName("테스트 환경이 정상적으로 작동하는지 확인합니다")
     void contextLoads() {
     }
 
