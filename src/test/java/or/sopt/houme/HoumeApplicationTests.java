bbc68ffdce6fc1413d798bc6482a1fcf5b2953e5
gdbs1107 <gdbs1107@naver.com>
2025-06-30 13:55:53 +0900

    chore:#13 jacoco 버전 업데이트

--- src/test/java/or/sopt/houme/HoumeApplicationTests.java
+++ src/test/java/or/sopt/houme/HoumeApplicationTests.java
@@ -1,6 +1,5 @@
 package or.sopt.houme;
 
-import jdk.jfr.Name;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
