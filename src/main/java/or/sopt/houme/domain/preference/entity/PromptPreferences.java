0191dc46e0f4de4b5e70d76be1e18b7f43d29513
gdbs1107 <gdbs1107@naver.com>
2025-07-06 01:08:18 +0900

    chore:#49 preference import 추가

--- src/main/java/or/sopt/houme/domain/preference/entity/PromptPreferences.java
+++ src/main/java/or/sopt/houme/domain/preference/entity/PromptPreferences.java
@@ -3,6 +3,7 @@ package or.sopt.houme.domain.preference.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import or.sopt.houme.domain.house.entity.Houses;
+import or.sopt.houme.domain.preference.entity.Preferences;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
