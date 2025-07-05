8915b890f26240a481b6da86f940b6304e253c56
gdbs1107 <gdbs1107@naver.com>
2025-07-06 01:30:14 +0900

    fix:#33 테이블 명 수정

--- src/main/java/or/sopt/houme/domain/user/repository/UserRepository.java
+++ src/main/java/or/sopt/houme/domain/user/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package or.sopt.houme.domain.user.repository;
 
-import or.sopt.houme.domain.user.entity.Users;
+import or.sopt.houme.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<Users, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
 
-    Optional<Users> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
