df2ec11d0c28e550a1f68b2fcf41ade593245357
gdbs1107 <gdbs1107@naver.com>
2025-07-06 15:50:00 +0900

    fix:#10 코드래빗 피드백 수정

--- src/main/java/or/sopt/houme/domain/openai/controller/OpenAiController.java
+++ src/main/java/or/sopt/houme/domain/openai/controller/OpenAiController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import or.sopt.houme.domain.openai.facade.OpenAiFacade;
 import or.sopt.houme.domain.openai.service.OpenAiService;
 import or.sopt.houme.domain.prompt.dto.PromptRequestDTO;
+import or.sopt.houme.global.api.ApiResponse;
 import or.sopt.houme.global.dto.ImageUploadResponseDTO;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,16 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/image")
 public class OpenAiController {
 
-    private final OpenAiFacade imageService;
+    private final OpenAiFacade openAiFacade;
 
     @Operation(summary = "chatgpt-image-1 을 활용한 이미지 저장 API",
         description = "실제 성능 테스트를 위한 메서드입니다. **호출 시, 서버 전재연에게 반드시 문의해주세요**")
     @GetMapping(value = "/generate")
-    public ResponseEntity<String> generate() {
+    public ResponseEntity<ApiResponse<String>> generate() {
 
-        ImageUploadResponseDTO responseDTO = imageService.makeImage(new PromptRequestDTO());
+        ImageUploadResponseDTO responseDTO = openAiFacade.makeImage(new PromptRequestDTO());
 
-        return ResponseEntity.ok().body(responseDTO.getImageLink());
+        return ResponseEntity.ok().body(ApiResponse.ok(responseDTO.getImageLink()));
     }
 
 }
