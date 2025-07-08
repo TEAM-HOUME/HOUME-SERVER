537ff35c8c28a1e17d150f233b25b7faa4286428
gdbs1107 <gdbs1107@naver.com>
2025-07-08 16:06:46 +0900

    feat:#63 api 명세 추가

--- src/main/java/or/sopt/houme/domain/user/controller/UserLandingController.java
+++ src/main/java/or/sopt/houme/domain/user/controller/UserLandingController.java
@@ -1,5 +1,7 @@
 package or.sopt.houme.domain.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Tag(name = "회원 랜딩페이지 관련 API")
 public class UserLandingController {
 
     private final UserLandingService userLandingService;
 
     @GetMapping("/check-has-generated-image")
+    @Operation(summary = "회원 이미지 생성 이력 조회 API",
+    description = "회원의 리프레시 토큰의 유효성과 이미지 생성 이력을 조회합니다 <br><br>" +
+            "이미지 생성 이력이 없으면 **false** 있다면 **true** 를 반환합니다")
     public ResponseEntity<ApiResponse<Boolean>> checkHasGeneratedImage(HttpServletRequest request) {
 
         Boolean result = userLandingService.getHasGeneratedImage(request);
