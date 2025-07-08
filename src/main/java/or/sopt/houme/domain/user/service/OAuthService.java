54498a93b46f1f314e86d38e66b44c0066513c11
Jeon JaeYeon <90432594+gdbs1107@users.noreply.github.com>
2025-07-08 23:45:42 +0900

    Fix/#68/social hotfix (#73)
    * feat:#68 yml 업데이트
    
    * fix
    
    * 로그 추가
    
    * 로그 추가

--- src/main/java/or/sopt/houme/domain/user/service/OAuthService.java
+++ src/main/java/or/sopt/houme/domain/user/service/OAuthService.java
@@ -66,15 +66,19 @@ public class OAuthService {
 
         log.info("aa:{}",kaKaoConfig.getRedirectUri());
 
+        log.info("aa:{}",accessCode);
         // 인가코드가 비어있다면 예외발생
         if (accessCode == null || accessCode.isEmpty()) {
             throw new UserException(ErrorCode.KAKAO_AUTH_CODE_INVALID);
         }
 
+        log.info("인가코드가 비어있지 않습니다");
         // 인가코드를 받고 그걸 통해서 인증 액세스 토큰을 발급받습니다
         KaKaoOAuthTokenDTO authorizationCode;
         try {
+            log.info("액세스 토큰 발급을 시작합니다");
             authorizationCode = getKaKaoOAuthTokenDTO(accessCode);
+            log.info("액세스 토큰을 발급받았습니다: {}", authorizationCode);
         } catch (FeignException e) {
             throw new UserException(ErrorCode.KAKAO_AUTH_CODE_INVALID);
         }
