50ac49aaaae55f71a6c6b082e859460ecc7f48ba
koobontak <kbt82883@gmail.com>
2025-07-08 22:11:53 +0900

    refactor#62: RequestMapping 어노테이션 추가

--- src/main/java/or/sopt/houme/domain/user/controller/UserController.java
+++ src/main/java/or/sopt/houme/domain/user/controller/UserController.java
@@ -8,14 +8,16 @@ import or.sopt.houme.global.api.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class UserController {
     private final UserService userService;
 
-    @GetMapping(value = "/api/v1/mypage/user")  // 유저의 이름, 사용가능한 크레딧 개수 조회
+    @GetMapping(value = "/mypage/user")  // 유저의 이름, 사용가능한 크레딧 개수 조회
     public ResponseEntity<ApiResponse<MyPageInfoResponse>> getMyPageInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
         MyPageInfoResponse myPageInfoResponse = userService.getMyPageInfo(userDetails.getUser());
 
