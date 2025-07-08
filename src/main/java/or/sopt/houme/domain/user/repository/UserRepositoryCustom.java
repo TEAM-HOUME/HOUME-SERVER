71f6c87be6d365304e93e52456f87b26b7280ca0
koobontak <kbt82883@gmail.com>
2025-07-09 01:46:59 +0900

    feat#62: 유저 이미지 생성 이력 조회 쿼리 작성

--- src/main/java/or/sopt/houme/domain/user/repository/UserRepositoryCustom.java
+++ src/main/java/or/sopt/houme/domain/user/repository/UserRepositoryCustom.java
@@ -1,5 +1,11 @@
 package or.sopt.houme.domain.user.repository;
 
+import or.sopt.houme.domain.user.controller.dto.UserImageHistoryDTO;
+
+import java.util.List;
+
 public interface UserRepositoryCustom {
     long countByMemberIdAndStatus(Long userId);
+
+    List<UserImageHistoryDTO> getUserImageHistory(Long userId);
 }
--- src/main/java/or/sopt/houme/domain/user/service/UserService.java
+++ src/main/java/or/sopt/houme/domain/user/service/UserService.java
@@ -2,12 +2,16 @@ package or.sopt.houme.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 import or.sopt.houme.domain.user.controller.dto.MyPageInfoResponse;
+import or.sopt.houme.domain.user.controller.dto.UserImageHistoryDTO;
+import or.sopt.houme.domain.user.controller.dto.UserImageHistoryListResponse;
 import or.sopt.houme.domain.user.entity.User;
 import or.sopt.houme.domain.user.repository.UserRepository;
 import or.sopt.houme.global.api.ErrorCode;
 import or.sopt.houme.global.api.handler.UserException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +24,12 @@ public class UserService {
         String name = findUser.getName();
         long creditCount = userRepository.countByMemberIdAndStatus(user.getId());
         return MyPageInfoResponse.of(name, creditCount);
+    }
+
+    @Transactional(readOnly = true)
+    public UserImageHistoryListResponse getUserImageHistoryList(User user) {
+        User findUser = userRepository.findById(user.getId()).orElseThrow(() -> new UserException(ErrorCode.USER_NOT_FOUND));
+        List<UserImageHistoryDTO> histories = userRepository.getUserImageHistory(findUser.getId());
+        return UserImageHistoryListResponse.of(histories);
     }
 }
