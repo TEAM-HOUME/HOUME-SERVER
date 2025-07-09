4f828acf6706c96e794b9af66b7bded0ae4629df
gdbs1107 <gdbs1107@naver.com>
2025-07-09 12:07:41 +0900

    feat:#10 PromptRequestDTO 구성

--- src/main/java/or/sopt/houme/domain/prompt/dto/PromptRequestDTO.java
+++ src/main/java/or/sopt/houme/domain/prompt/dto/PromptRequestDTO.java
@@ -1,7 +1,19 @@
 package or.sopt.houme.domain.prompt.dto;
 
 import lombok.Getter;
+import or.sopt.houme.domain.house.entity.enums.Equilibrium;
 
 @Getter
 public class PromptRequestDTO {
+
+    /**
+     * 도면 식별자
+     * 평형 식별자
+     * 무드 식별자
+     * 가구 식별자
+     * */
+    private Long floorPlanId;
+    private Long tasteId;
+    private Equilibrium equilibrium;
+
 }
