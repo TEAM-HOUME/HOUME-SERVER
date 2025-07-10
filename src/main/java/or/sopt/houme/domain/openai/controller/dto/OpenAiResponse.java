c38f80af40511f61e356a7be4b8182250c69f938
gdbs1107 <gdbs1107@naver.com>
2025-07-10 10:53:14 +0900

    chore:#93 @NoArgsConstructor 어노테이션 추가

--- src/main/java/or/sopt/houme/domain/openai/controller/dto/OpenAiResponse.java
+++ src/main/java/or/sopt/houme/domain/openai/controller/dto/OpenAiResponse.java
@@ -3,12 +3,14 @@ package or.sopt.houme.domain.openai.controller.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
+@NoArgsConstructor
 public class OpenAiResponse {
     private List<ImageData> data;
 
