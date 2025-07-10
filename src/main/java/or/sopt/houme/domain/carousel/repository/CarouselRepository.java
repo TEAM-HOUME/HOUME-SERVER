bd59ee3c478a71b6a682bcc5b4fdf9f2032a2e26
gdbs1107 <gdbs1107@naver.com>
2025-07-10 11:08:18 +0900

    feat:#94 carousel repository 생성

---
+++ src/main/java/or/sopt/houme/domain/carousel/repository/CarouselRepository.java
@@ -0,0 +1,7 @@
+package or.sopt.houme.domain.carousel.repository;
+
+import or.sopt.houme.domain.carousel.entity.Carousel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarouselRepository extends JpaRepository<Carousel, Long> {
+}
