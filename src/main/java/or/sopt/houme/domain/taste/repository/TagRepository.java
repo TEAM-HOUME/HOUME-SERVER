c691969753c1d8f8100c63109e5487cbd24af431
koobontak <kbt82883@gmail.com>
2025-07-10 17:14:16 +0900

    feat#97: 사용자와 이미지에 따른 Tag객체 조회

---
+++ src/main/java/or/sopt/houme/domain/taste/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package or.sopt.houme.domain.taste.repository;
+
+import or.sopt.houme.domain.taste.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long>, TagRepositoryCustom {
+}
