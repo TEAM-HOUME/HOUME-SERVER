32653b676f66b55a36591b6ab3a345a542a73c8d
gdbs1107 <gdbs1107@naver.com>
2025-07-10 22:34:52 +0900

    feat:#94 락 스레드 sleep 시간 지수적으로 증가하도록 수정'

--- src/main/java/or/sopt/houme/domain/carousel/facade/CarouselOptimisticLockFacade.java
+++ src/main/java/or/sopt/houme/domain/carousel/facade/CarouselOptimisticLockFacade.java
@@ -24,6 +24,7 @@ public class CarouselOptimisticLockFacade {
      *
      * 실행 중 @Version 이 일치하지 않으면 끝까지 실행된 트랜잭션을 이전으로 롤백시키는 방식으로 동시성을 제어합니다
      * 동시성에 걸리면 스레드를 일정시간 sleep 시켜놓습니다
+     * sleep 되는 시간은 지수적으로 증가하여 DB에 불필요한 접근을 방지합니다
      * 스레드가 sleep 된 횟수가 일정횟수에 도달하면 예외를 발생시킵니다
      *
      * 이는 hateCarousel() 도 동일합니다
@@ -36,13 +37,15 @@ public class CarouselOptimisticLockFacade {
                 preferenceService.likeCarousel(user, carouselId);
                 return;
             } catch (OptimisticLockException | DataIntegrityViolationException e) {
+                long backoffTime = (long) Math.pow(2, retryCount) * RETRY_DELAY_MS;
+                Thread.sleep(backoffTime);
                 retryCount++;
-                Thread.sleep(RETRY_DELAY_MS);
             }
         }
 
         throw new CarouselException(ErrorCode.CAROUSEL_RETRY_EXCEPTION);
     }
+
 
     public void hateCarousel(User user, Long carouselId) throws InterruptedException {
         int retryCount = 0;
@@ -52,8 +55,9 @@ public class CarouselOptimisticLockFacade {
                 preferenceService.hateCarousel(user, carouselId);
                 return;
             } catch (OptimisticLockException | DataIntegrityViolationException e) {
+                long backoffTime = (long) Math.pow(2, retryCount) * RETRY_DELAY_MS;
+                Thread.sleep(backoffTime);
                 retryCount++;
-                Thread.sleep(RETRY_DELAY_MS);
             }
         }
 
