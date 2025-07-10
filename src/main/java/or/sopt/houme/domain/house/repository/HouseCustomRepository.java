d044f88a168671fab98e6fd423503106a39de1be
koobontak <kbt82883@gmail.com>
2025-07-10 17:13:52 +0900

    feat#97: 사용자와 이미지에 따른 House객체 조회

--- src/main/java/or/sopt/houme/domain/house/repository/HouseCustomRepository.java
+++ src/main/java/or/sopt/houme/domain/house/repository/HouseCustomRepository.java
@@ -3,7 +3,11 @@ package or.sopt.houme.domain.house.repository;
 import or.sopt.houme.domain.house.entity.House;
 import or.sopt.houme.domain.user.entity.User;
 
+import java.util.Optional;
+
 public interface HouseCustomRepository {
 
     House findLatestHouse(User user);
+
+    Optional<House> findHouseByUserIdAndImageId(Long userId, Long imageId);
 }
--- src/main/java/or/sopt/houme/domain/house/repository/HouseCustomRepositoryImpl.java
+++ src/main/java/or/sopt/houme/domain/house/repository/HouseCustomRepositoryImpl.java
@@ -1,12 +1,14 @@
 package or.sopt.houme.domain.house.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import or.sopt.houme.domain.generateImage.entity.QGenerateImage;
 import or.sopt.houme.domain.house.entity.House;
 import or.sopt.houme.domain.house.entity.QHouse;
 import or.sopt.houme.domain.user.entity.User;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -24,5 +26,23 @@ public class HouseCustomRepositoryImpl implements HouseCustomRepository {
                 .orderBy(qHouse.id.desc())
                 .limit(1)
                 .fetchOne();
+    }
+
+    @Override
+    public Optional<House> findHouseByUserIdAndImageId(Long userId, Long imageId) {
+        QHouse house = QHouse.house;
+        QGenerateImage generateImage = QGenerateImage.generateImage;
+
+        return Optional.ofNullable(
+                queryFactory
+                        .selectFrom(house)
+                        .join(generateImage)
+                        .on(generateImage.house.eq(house))
+                        .where(
+                                house.user.id.eq(userId),
+                                generateImage.id.eq(imageId)
+                        )
+                        .fetchOne()
+        );
     }
 }
