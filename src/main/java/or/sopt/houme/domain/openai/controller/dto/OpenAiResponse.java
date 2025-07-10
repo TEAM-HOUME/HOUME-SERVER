bfc570f9a181d987a7916f9edd8cdd2004337862
gdbs1107 <gdbs1107@naver.com>
2025-07-10 10:54:41 +0900

    chore#93 @Data -> @Getter 로 수정

--- src/main/java/or/sopt/houme/domain/openai/controller/dto/OpenAiResponse.java
+++ src/main/java/or/sopt/houme/domain/openai/controller/dto/OpenAiResponse.java
@@ -3,18 +3,19 @@ package or.sopt.houme.domain.openai.controller.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Data
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class OpenAiResponse {
     private List<ImageData> data;
 
-    @Data
+    @Getter
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class ImageData {
         private String url;
