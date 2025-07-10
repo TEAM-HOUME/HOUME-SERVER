f898d810bb48403878e24dc8176ea25e5ba9c639
임채륜 <59529734+PBEM22@users.noreply.github.com>
2025-07-10 18:07:50 +0900

    [FEAT] cursor기반 무드보드 제공 API 개발 (#99)
    * feat: #98 무드보드 제공 API 개발
    
    * fix: valid 검증 추가
    
    * fix: valid 검증 추가

---
+++ src/main/java/or/sopt/houme/domain/taste/controller/TasteController.java
@@ -0,0 +1,37 @@
+package or.sopt.houme.domain.taste.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
+import or.sopt.houme.domain.taste.dto.response.MoodBoardListResponse;
+import or.sopt.houme.domain.taste.service.TasteService;
+import or.sopt.houme.global.api.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@Tag(name = "무드보드 API")
+@RequiredArgsConstructor
+public class TasteController {
+
+    private final TasteService tasteService;
+
+    @Operation(summary = "무드보드 제공 API",
+            description = "무드보드 전체를 조회합니다. (커서 기반 페이지네이션)")
+    @GetMapping("/moodboard-images")
+    public ResponseEntity<ApiResponse<MoodBoardListResponse>> moodboardImages(
+            @RequestParam(required = false) Long cursor,
+            @RequestParam(defaultValue = "10") @Valid @Min(1) int limit
+    ) {
+
+        MoodBoardListResponse moodboard = tasteService.getMoodboard(cursor, limit);
+
+        return ResponseEntity.ok(ApiResponse.ok(moodboard));
+    }
+}
---
+++ src/main/java/or/sopt/houme/domain/taste/repository/TasteCustomRepositoryImpl.java
@@ -0,0 +1,40 @@
+package or.sopt.houme.domain.taste.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import or.sopt.houme.domain.taste.entity.Taste;
+import org.springframework.stereotype.Repository;
+import or.sopt.houme.domain.taste.entity.QTaste;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class TasteCustomRepositoryImpl implements TasteCustomRepository{
+
+    private final JPAQueryFactory queryFactory;
+
+    // 커서기반 페이지네이션
+    @Override
+    public List<Taste> findTasteByCursor(Long cursorId, int size) {
+
+        QTaste qTaste = QTaste.taste;
+
+        return queryFactory
+                .selectFrom(qTaste)
+                .where(ltCursorId(cursorId))
+                .orderBy(qTaste.id.desc())
+                .limit(size)
+                .fetch();
+
+        // 아래는 오래된 순서부터
+//                .where(gtCursorId(cursorId)) // 커서 기준 ID보다 큰 데이터 = 더 최신
+//                .orderBy(qTaste.id.asc())    // 오래된 순
+    }
+
+    // 조건 검사
+    private BooleanExpression ltCursorId(Long cursorId) {
+        return cursorId != null ? QTaste.taste.id.lt(cursorId) : null;
+    }
+}
