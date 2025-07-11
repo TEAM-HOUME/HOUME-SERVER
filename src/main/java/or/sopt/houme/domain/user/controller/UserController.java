9f6ee62e497fcdca5589dc15581407aa6c7c7374
koobontak <kbt82883@gmail.com>
2025-07-11 15:54:17 +0900

    refactor#107: 명세서 설명 추가

--- src/main/java/or/sopt/houme/domain/user/controller/UserController.java
+++ src/main/java/or/sopt/houme/domain/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package or.sopt.houme.domain.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import or.sopt.houme.domain.user.controller.dto.*;
@@ -13,10 +15,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
+@Tag(name = "회원 관련 api")
 public class UserController {
     private final UserService userService;
 
     @GetMapping(value = "/mypage/user")  // 유저의 이름, 사용가능한 크레딧 개수 조회
+    @Operation(summary = "마이페이지 기본 정보 제공 API")
     public ResponseEntity<ApiResponse<MyPageInfoResponse>> getMyPageInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
         MyPageInfoResponse myPageInfoResponse = userService.getMyPageInfo(userDetails.getUser());
 
@@ -24,6 +28,7 @@ public class UserController {
     }
 
     @GetMapping(value = "/mypage/images")
+    @Operation(summary = "마이페이지 이미지 생성 이력 제공 API")
     public ResponseEntity<ApiResponse<UserImageHistoryListResponse>> getUserImageHistoryList(@AuthenticationPrincipal CustomUserDetails userDetails) {
         UserImageHistoryListResponse userImageHistoryListResponse = userService.getUserImageHistoryList(userDetails.getUser());
 
@@ -31,6 +36,7 @@ public class UserController {
     }
 
     @GetMapping(value = "/mypage/images/{imageId}")
+    @Operation(summary = "마이페이지에서 이미지 생성 이력 클릭시 결과 페이지 제공 API")
     public ResponseEntity<ApiResponse<ImageHistoryResultPageResponse>> getImageHistoryResultPage(@AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable Long imageId) {
         ImageHistoryResultPageResponse imageHistoryResultPageResponse = userService.getImageHistoryResultPage(userDetails.getUser(), imageId);
 
@@ -38,6 +44,7 @@ public class UserController {
     }
 
     @PutMapping(value = "/signup")
+    @Operation(summary = "자체 회원가입 API")
     public ResponseEntity<ApiResponse<Void>> updateUser(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody @Valid CreateUserRequest createUserRequest) {
         userService.updateUser(userDetails.getUser(), createUserRequest);
 
