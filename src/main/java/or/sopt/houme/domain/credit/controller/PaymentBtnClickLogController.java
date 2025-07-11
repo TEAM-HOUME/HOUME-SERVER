5d3c11d8548beb57431278e33913f838a30fa837
bontak <142137769+kbt82883@users.noreply.github.com>
2025-07-11 13:45:49 +0900

    [FEATURE] 충전하기 버튼 클릭 로그 저장 (#106)
    * feat#103: PaymentBtnClickLog 엔티티 추가
    
    * feat#103: 충전하기 버튼 클릭 로그 저장 api 구현
    
    * refactor#103: Operation 추가
    
    * fix#103: 화이트리스트 체크
    
    * fix#103: Transctional 추가

---
+++ src/main/java/or/sopt/houme/domain/credit/controller/PaymentBtnClickLogController.java
@@ -0,0 +1,26 @@
+package or.sopt.houme.domain.credit.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import or.sopt.houme.domain.credit.service.PaymentBtnClickLogService;
+import or.sopt.houme.domain.user.controller.dto.CustomUserDetails;
+import or.sopt.houme.global.api.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@Tag(name = "충전하기 버튼 클릭 로그 API")
+@RequiredArgsConstructor
+public class PaymentBtnClickLogController {
+    private final PaymentBtnClickLogService paymentBtnClickLogService;
+
+    @PostMapping(value = "/credits/logs")
+    @Operation(summary = "충전하기 모달창에서 버튼 클릭시 로그 저장 api")
+    public ResponseEntity<ApiResponse<Void>> createPaymentBtnClickLog(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        paymentBtnClickLogService.createPaymentBtnClickLog(userDetails.getUser());
+        return ResponseEntity.ok(ApiResponse.ok(null));
+    }
+}
