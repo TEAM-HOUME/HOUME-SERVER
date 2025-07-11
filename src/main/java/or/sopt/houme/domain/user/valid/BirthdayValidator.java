dab3ff90784f96b834b78cf58985b65c164b6e11
koobontak <kbt82883@gmail.com>
2025-07-11 15:50:05 +0900

    feat#107: 자체 회원가입 요청 dto 및 입력값 검증 추가

---
+++ src/main/java/or/sopt/houme/domain/user/valid/BirthdayValidator.java
@@ -0,0 +1,26 @@
+package or.sopt.houme.domain.user.valid;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.time.LocalDate;
+
+public class BirthdayValidator implements ConstraintValidator<ValidBirthday, LocalDate> {
+
+    @Override
+    public boolean isValid(LocalDate birthday, ConstraintValidatorContext context) {
+        // 생년월일 비어있으면 false
+        if (birthday == null) return false;
+
+        LocalDate today = LocalDate.now();
+
+        // 1900년보다 이전이거나 오늘 이후면 false
+        if (birthday.getYear() < 1900 || birthday.isAfter(today)) {
+            return false;
+        }
+
+        // 만 14세 이하는 false
+        LocalDate minimumDate = today.minusYears(14);
+        return !birthday.isAfter(minimumDate);
+    }
+}
