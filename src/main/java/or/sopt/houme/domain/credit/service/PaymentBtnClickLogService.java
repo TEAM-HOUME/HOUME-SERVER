a8f6924976f675200b6ea1790409e777cf19108b
koobontak <kbt82883@gmail.com>
2025-07-11 13:42:44 +0900

    fix#103: Transctional 추가

--- src/main/java/or/sopt/houme/domain/credit/service/PaymentBtnClickLogService.java
+++ src/main/java/or/sopt/houme/domain/credit/service/PaymentBtnClickLogService.java
@@ -5,9 +5,11 @@ import or.sopt.houme.domain.credit.entity.PaymentBtnClickLog;
 import or.sopt.houme.domain.credit.repository.PaymentBtnClickLogRepository;
 import or.sopt.houme.domain.user.entity.User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class PaymentBtnClickLogService {
     private final PaymentBtnClickLogRepository paymentBtnClickLogRepository;
 
