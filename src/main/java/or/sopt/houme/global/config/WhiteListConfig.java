75fb45fbbe6ec36919f56ba53928966cb20e54fd
gdbs1107 <gdbs1107@naver.com>
2025-07-13 17:45:42 +0900

    feat:#128 프로메테우스 메트릭 수집

--- src/main/java/or/sopt/houme/global/config/WhiteListConfig.java
+++ src/main/java/or/sopt/houme/global/config/WhiteListConfig.java
@@ -57,7 +57,8 @@ public class WhiteListConfig {
     // 모니터링 관련 인가 설정
     public static final List<String> monitoringWhiteList() {
         return List.of(
-                "/actuator/prometheus"
+                "/actuator/prometheus",
+                "/actuator/**"
         );
     }
 }
