2f1ec15ed4b8e88ff8d30dfbd03b7b15e048b5d6
pbem22 <dlacofbs1229@gmail.com>
2025-07-15 15:29:07 +0900

    fix: #155 null 처리

--- src/main/java/or/sopt/houme/domain/taste/service/TasteTagServiceImpl.java
+++ src/main/java/or/sopt/houme/domain/taste/service/TasteTagServiceImpl.java
@@ -2,6 +2,8 @@ package or.sopt.houme.domain.taste.service;
 
 import lombok.RequiredArgsConstructor;
 import or.sopt.houme.domain.taste.repository.taste_tag.TasteTagRepository;
+import or.sopt.houme.global.api.ErrorCode;
+import or.sopt.houme.global.api.GeneralException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,9 +17,15 @@ public class TasteTagServiceImpl implements TasteTagService {
     // 우선순위 가장 높은 TasteId 반환
     @Override
     public Long getPriorityId(List<Long> ids) {
+        if (ids.isEmpty()) {
+            throw new GeneralException(ErrorCode.NOT_VALID_EXCEPTION);
+        }
 
         Long bestTasteId = tasteTagRepository.findBestTasteId(ids);
 
+        if (bestTasteId == null) {
+            throw new GeneralException(ErrorCode.NOT_FOUND_TASTE);
+        }
         return bestTasteId;
     }
 }
