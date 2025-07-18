302bd27123d211ddd618142faeef452831f39b95
pbem22 <dlacofbs1229@gmail.com>
2025-07-19 04:20:02 +0900

    fix: #219 이미지 좋아요 조회 로직 수정

--- src/main/java/or/sopt/houme/domain/preference/repository/PromptPreferenceRepository.java
+++ src/main/java/or/sopt/houme/domain/preference/repository/PromptPreferenceRepository.java
@@ -2,6 +2,10 @@ package or.sopt.houme.domain.preference.repository;
 
 import or.sopt.houme.domain.preference.entity.PromptPreference;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PromptPreferenceRepository extends JpaRepository<PromptPreference, Long> {
+
+    @Query("SELECT pp FROM PromptPreference pp WHERE pp.house.id = :houseId ORDER BY pp.id DESC")
+    PromptPreference findTopByHouseIdOrderByIdDesc(Long houseId);
 }
