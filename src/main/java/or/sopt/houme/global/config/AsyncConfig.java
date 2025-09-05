b807f85d326dd072d7daa6ad1b9ce71d78025130
pbem22 <dlacofbs1229@gmail.com>
2025-09-05 17:14:41 +0900

    hotfix: 스레드풀 감축

--- src/main/java/or/sopt/houme/global/config/AsyncConfig.java
+++ src/main/java/or/sopt/houme/global/config/AsyncConfig.java
@@ -15,9 +15,9 @@ public class AsyncConfig {
     @Bean(name = "imageGenerationExecutor")
     public Executor imageGenerationExecutor(){
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(4);        // 동시에 실행할 기본 스레드 수, JVM 기본 스레드와 별도
-        executor.setMaxPoolSize(8);         // 최대 스레드 수
-        executor.setQueueCapacity(10);     // 대기 큐 크기
+        executor.setCorePoolSize(2);        // 동시에 실행할 기본 스레드 수, JVM 기본 스레드와 별도
+        executor.setMaxPoolSize(2);         // 최대 스레드 수
+        executor.setQueueCapacity(5);     // 대기 큐 크기
         executor.setThreadNamePrefix("ImageGenerator-");
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
