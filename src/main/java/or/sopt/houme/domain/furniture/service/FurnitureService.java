80cc477a3baed4fba618a8d421378f01bfff857c
koobontak <kbt82883@gmail.com>
2025-10-04 18:46:16 +0900

    chore#246: import문 정리

--- src/main/java/or/sopt/houme/domain/furniture/service/FurnitureService.java
+++ src/main/java/or/sopt/houme/domain/furniture/service/FurnitureService.java
@@ -1,9 +1,7 @@
 package or.sopt.houme.domain.furniture.service;
 
-import or.sopt.houme.domain.furniture.dto.external.naverShop.FurnitureProductsInfoResponse;
 import or.sopt.houme.domain.furniture.dto.response.FurnitureAndActivityResponse;
 import or.sopt.houme.domain.furniture.dto.response.FurnitureCategoriesResponse;
-import or.sopt.houme.domain.furniture.dto.external.naverShop.FurnitureProductsInfoResponseForPlan;
 import or.sopt.houme.domain.furniture.entity.FurnitureTag;
 import or.sopt.houme.domain.user.entity.User;
 
