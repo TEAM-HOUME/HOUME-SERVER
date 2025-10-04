fab0ff0a0f997b56e462911ed7f62e75bc6c6d9d
koobontak <kbt82883@gmail.com>
2025-10-04 23:41:34 +0900

    feat#246: 네이버 검색 응답 기획전달용 서비스 로직 추가

--- src/main/java/or/sopt/houme/domain/furniture/service/ImageHashService.java
+++ src/main/java/or/sopt/houme/domain/furniture/service/ImageHashService.java
@@ -5,7 +5,10 @@ import lombok.RequiredArgsConstructor;
 import or.sopt.houme.domain.furniture.client.FastApiImageHashClient;
 import or.sopt.houme.domain.furniture.dto.external.fastApiImagehash.ImageHashRequest;
 import or.sopt.houme.domain.furniture.dto.external.fastApiImagehash.SimilarityResponse;
+import or.sopt.houme.domain.furniture.dto.external.fastApiImagehash.forPlan.ImageHashRequestForPlan;
+import or.sopt.houme.domain.furniture.dto.external.fastApiImagehash.forPlan.SimilarityResponseForPlan;
 import or.sopt.houme.domain.furniture.dto.external.naverShop.FurnitureProductsInfoResponse;
+import or.sopt.houme.domain.furniture.dto.external.naverShop.forPlan.FurnitureProductsInfoResponseForPlan;
 import or.sopt.houme.domain.furniture.dto.external.naverShop.NaverFurnitureProductDto;
 import or.sopt.houme.global.api.ErrorCode;
 import or.sopt.houme.global.api.handler.FastApiException;
@@ -54,6 +57,67 @@ public class ImageHashService {
                                 product.furnitureProductName(),
                                 product.furnitureProductMallName(),
                                 product.furnitureProductId(),
+                                r.similarity()
+                        );
+                    })
+                    .toList();
+        } catch (FeignException e) {
+            int status = e.status();
+
+            // 4xx 에러 → 클라이언트 오류
+            if (status >= 400 && status < 500) {
+                throw new FastApiException(ErrorCode.IMAGE_HASH_CLIENT_ERROR);
+            }
+
+            // 5xx 에러 → 서버 오류
+            if (status >= 500) {
+                throw new FastApiException(ErrorCode.IMAGE_HASH_SERVER_ERROR);
+            }
+
+            // 그 외 예외
+            throw new FastApiException(ErrorCode.IMAGE_HASH_SERVER_ERROR);
+
+        } catch (Exception e) {
+            throw new FastApiException(ErrorCode.IMAGE_HASH_SERVER_ERROR);
+        }
+    }
+
+
+    // 기획의사결정용
+    public List<FurnitureProductsInfoResponseForPlan.FurnitureProductInfo> rankByImageSimilarityForPlan(
+            String baseImageUrl,
+            List<NaverFurnitureProductDto> products,
+            int pHash,
+            int colorHash,
+            int topN) {
+        try {
+            // FastAPI에 요청 보낼 DTO 구성
+            ImageHashRequestForPlan request = ImageHashRequestForPlan.of(baseImageUrl, products, pHash, colorHash);
+
+            // 외부 API 호출
+            SimilarityResponseForPlan response = fastApiImageHashClient.getTopKSimilarImagesForPlan(request);
+
+            if (response == null || response.rankedProducts() == null) {
+                throw new FastApiException(ErrorCode.IMAGE_HASH_EMPTY_RESPONSE);
+            }
+
+            // productId로 빠르게 매핑
+            Map<Long, NaverFurnitureProductDto> productMap = products.stream()
+                    .collect(Collectors.toMap(NaverFurnitureProductDto::furnitureProductId, Function.identity()));
+
+            // FastAPI 응답 기반으로 상위 N개 상품 매핑
+            return response.rankedProducts().stream()
+                    .limit(topN)
+                    .map(r -> {
+                        var product = productMap.get(r.productId());
+
+                        return FurnitureProductsInfoResponseForPlan.FurnitureProductInfo.of(
+                                baseImageUrl,
+                                product.furnitureProductImageUrl(),
+                                product.furnitureProductSiteUrl(),
+                                product.furnitureProductName(),
+                                product.furnitureProductMallName(),
+                                String.valueOf(product.furnitureProductId()),
                                 r.similarity()
                         );
                     })
