c807f38c775cf8b44e397a9f91331f9fef0ed582
koobontak <kbt82883@gmail.com>
2025-10-04 23:40:40 +0900

    feat#246: 네이버 검색 응답 기획전달용 엔드포인트 추가

--- src/main/java/or/sopt/houme/domain/furniture/facade/FurnitureFacadeImpl.java
+++ src/main/java/or/sopt/houme/domain/furniture/facade/FurnitureFacadeImpl.java
@@ -2,6 +2,7 @@ package or.sopt.houme.domain.furniture.facade;
 
 import lombok.RequiredArgsConstructor;
 import or.sopt.houme.domain.furniture.dto.external.naverShop.FurnitureProductsInfoResponse;
+import or.sopt.houme.domain.furniture.dto.external.naverShop.forPlan.FurnitureProductsInfoResponseForPlan;
 import or.sopt.houme.domain.furniture.dto.external.naverShop.NaverFurnitureProductDto;
 import or.sopt.houme.domain.furniture.entity.FurnitureTag;
 import or.sopt.houme.domain.furniture.service.FurnitureService;
@@ -34,5 +35,22 @@ public class FurnitureFacadeImpl implements FurnitureFacade {
 
         // 4. 최종 응답 조립 (Facade 책임)
         return FurnitureProductsInfoResponse.of(user.getName(), infos);
+    }
+
+    // 기획의사결정용
+    @Override
+    public FurnitureProductsInfoResponseForPlan getFurnitureProductInfoFromNaverApiForPlan(User user, Long tagId, Long furnitureId, String searchKeyword, int pHash, int colorHash, int display) {
+        // 1. FurnitureTag 조회 (DB)
+        FurnitureTag furnitureTag = furnitureService.findFurnitureTagForPlan(tagId, furnitureId);
+
+        // 2. 네이버 API 호출
+        List<NaverFurnitureProductDto> products = naverShopService.search(searchKeyword, 100);
+
+        // 3. FastAPI 호출 → 유사도 기반 상위 상품 리스트만 반환
+        List<FurnitureProductsInfoResponseForPlan.FurnitureProductInfo> infos =
+                imageHashService.rankByImageSimilarityForPlan(furnitureTag.getFurnitureUrl(), products, pHash, colorHash, display);
+
+        // 4. 최종 응답 조립 (Facade 책임)
+        return FurnitureProductsInfoResponseForPlan.of(user.getName(), infos);
     }
 }
