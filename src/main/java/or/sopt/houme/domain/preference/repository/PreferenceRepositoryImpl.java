7d7690006207f69615cce37ac7e021e266ccffe2
pbem22 <dlacofbs1229@gmail.com>
2025-10-05 23:40:56 +0900

    fix: findPreferenceByUserIdAndImageId promptPreferecne -> generateImagePreference로 수정

--- src/main/java/or/sopt/houme/domain/preference/repository/PreferenceRepositoryImpl.java
+++ src/main/java/or/sopt/houme/domain/preference/repository/PreferenceRepositoryImpl.java
@@ -5,8 +5,8 @@ import lombok.RequiredArgsConstructor;
 import or.sopt.houme.domain.generateImage.entity.QGenerateImage;
 import or.sopt.houme.domain.house.entity.QHouse;
 import or.sopt.houme.domain.preference.entity.Preference;
+import or.sopt.houme.domain.preference.entity.QGenerateImagePreference;
 import or.sopt.houme.domain.preference.entity.QPreference;
-import or.sopt.houme.domain.preference.entity.QPromptPreference;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -19,15 +19,15 @@ public class PreferenceRepositoryImpl implements PreferenceRepositoryCustom {
     @Override
     public Optional<Preference> findPreferenceByUserIdAndImageId(Long userId, Long imageId) {
         QPreference preference = QPreference.preference;
-        QPromptPreference promptPreference = QPromptPreference.promptPreference;
+        QGenerateImagePreference generateImagePreference = QGenerateImagePreference.generateImagePreference;
         QHouse house = QHouse.house;
         QGenerateImage generateImage = QGenerateImage.generateImage;
 
         return Optional.ofNullable(queryFactory
                 .selectFrom(preference)
-                .join(promptPreference).on(promptPreference.preference.eq(preference))
-                .join(house).on(promptPreference.house.eq(house))
-                .join(generateImage).on(generateImage.house.eq(house))
+                .join(preference).on(generateImagePreference.preference.eq(preference)).fetchJoin()
+                .join(generateImage).on(generateImagePreference.generateImage.eq(generateImage)).fetchJoin()
+                .join(generateImage).on(generateImage.house.eq(house)).fetchJoin()
                 .where(
                         house.user.id.eq(userId),
                         generateImage.id.eq(imageId)
