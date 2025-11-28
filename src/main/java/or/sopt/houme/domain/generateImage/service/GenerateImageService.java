5461a6ba9b58645b7d7d038098b802e6cf810bd9
pbem22 <dlacofbs1229@gmail.com>
2025-11-28 16:30:28 +0900

    refactor: #341 B안 이미지(1장) 생성 트랜잭션 분리

--- src/main/java/or/sopt/houme/domain/generateImage/service/GenerateImageService.java
+++ src/main/java/or/sopt/houme/domain/generateImage/service/GenerateImageService.java
@@ -3,10 +3,12 @@ package or.sopt.houme.domain.generateImage.service;
 import or.sopt.houme.domain.generateImage.entity.GenerateImage;
 import or.sopt.houme.domain.house.entity.House;
 import or.sopt.houme.global.dto.ImageUploadResponseDTO;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface GenerateImageService {
 
     // 이미지 생성
+    @Transactional
     GenerateImage createGenerateImage(ImageUploadResponseDTO request, House house);
 
     // 이미지 ID로 조회
