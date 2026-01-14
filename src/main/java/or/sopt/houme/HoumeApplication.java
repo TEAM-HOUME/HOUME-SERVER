737f848fc62555195d55206457182d3a4bbc8cc2
gdbs1107 <gdbs1107@naver.com>
2026-01-14 11:56:03 +0900

    chore:#397 큐레이션 배치 스케줄러 추가

--- src/main/java/or/sopt/houme/HoumeApplication.java
+++ src/main/java/or/sopt/houme/HoumeApplication.java
@@ -3,9 +3,11 @@ package or.sopt.houme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class HoumeApplication {
 
     public static void main(String[] args) {
