5e901fb13a7320386b89f22a98df5b2739a603e2
gdbs1107 <gdbs1107@naver.com>
2026-03-14 13:24:04 +0900

    fix:#440 어드민 컨트롤러 테스트 보안 문맥 반영

--- src/test/java/or/sopt/houme/domain/user/presentation/admin/controller/AdminTagControllerTest.java
+++ src/test/java/or/sopt/houme/domain/user/presentation/admin/controller/AdminTagControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import or.sopt.houme.domain.user.presentation.admin.controller.dto.AdminTagUpdateRequestDTO;
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
 @ActiveProfiles("test")
+@WithMockUser(roles = "ADMIN")
 class AdminTagControllerTest {
 
     @Autowired
