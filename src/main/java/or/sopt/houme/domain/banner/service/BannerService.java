f100761dce9c6ae303cb892489a928686cc73d6d
koobontak <kbt82883@gmail.com>
2026-03-25 16:08:09 +0900

    chore#451: 엔드포인트 수정

--- src/main/java/or/sopt/houme/domain/banner/service/BannerService.java
+++ src/main/java/or/sopt/houme/domain/banner/service/BannerService.java
@@ -6,7 +6,7 @@ import or.sopt.houme.domain.banner.presentation.dto.response.LandingListResponse
 import or.sopt.houme.domain.banner.presentation.dto.response.OtherStyleDetailResponse;
 import or.sopt.houme.domain.banner.presentation.dto.response.OtherStyleListResponse;
 
-public interface BannerExploreService {
+public interface BannerService {
     LandingListResponse getLandings();
 
     BannerExploreListResponse getExploreBanners(Long bannerId);
