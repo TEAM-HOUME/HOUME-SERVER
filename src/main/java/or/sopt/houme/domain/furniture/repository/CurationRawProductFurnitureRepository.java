5da1468623628b4ea19909bf8e6e0b5846ca3399
gdbs1107 <gdbs1107@naver.com>
2026-05-22 14:21:10 +0900

    refactor:#524 RAW 상품 가구 매핑 조회 QueryDSL 적용

--- src/main/java/or/sopt/houme/domain/furniture/repository/CurationRawProductFurnitureRepository.java
+++ src/main/java/or/sopt/houme/domain/furniture/repository/CurationRawProductFurnitureRepository.java
@@ -3,25 +3,11 @@ package or.sopt.houme.domain.furniture.repository;
 import or.sopt.houme.domain.furniture.model.entity.CurationRawProduct;
 import or.sopt.houme.domain.furniture.model.entity.CurationRawProductFurniture;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface CurationRawProductFurnitureRepository extends JpaRepository<CurationRawProductFurniture, Long> {
-
-    @Query("""
-            select mapping
-            from CurationRawProductFurniture mapping
-            join fetch mapping.furniture furniture
-            join fetch furniture.furnitureType furnitureType
-            where mapping.curationRawProduct.id in :rawProductIds
-            """)
-    List<CurationRawProductFurniture> findAllByCurationRawProductIdInWithFurniture(
-            @Param("rawProductIds") List<Long> rawProductIds
-    );
+public interface CurationRawProductFurnitureRepository
+        extends JpaRepository<CurationRawProductFurniture, Long>, CurationRawProductFurnitureRepositoryCustom {
 
     void deleteAllByCurationRawProduct(CurationRawProduct curationRawProduct);
 }
