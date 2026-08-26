c55bb6d93d41b94956182be14eade1f04a025394
koobontak <kbt82883@gmail.com>
2026-08-26 21:02:40 +0900

    refactor:#635 쿠팡 키워드 상품 삭제 QueryDSL 전환

--- houme-infra/src/main/java/or/sopt/houme/domain/coupang/repository/CoupangKeywordProductJpaRepository.java
+++ houme-infra/src/main/java/or/sopt/houme/domain/coupang/repository/CoupangKeywordProductJpaRepository.java
@@ -2,13 +2,7 @@ package or.sopt.houme.domain.coupang.repository;
 
 import or.sopt.houme.domain.coupang.model.entity.CoupangKeywordProductJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface CoupangKeywordProductJpaRepository extends JpaRepository<CoupangKeywordProductJpaEntity, Long> {
-
-    @Modifying(flushAutomatically = true)
-    @Query("delete from CoupangKeywordProductJpaEntity mapping where mapping.keyword.id = :keywordId")
-    void deleteByKeywordId(@Param("keywordId") Long keywordId);
+public interface CoupangKeywordProductJpaRepository extends JpaRepository<CoupangKeywordProductJpaEntity, Long>,
+        CoupangKeywordProductJpaRepositoryCustom {
 }
