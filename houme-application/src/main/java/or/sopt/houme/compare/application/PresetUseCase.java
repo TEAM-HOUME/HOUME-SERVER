78c41ad7d0ccb5534cbd158c2923d6fbca4f86b6
pbem22 <dlacofbs1229@gmail.com>
2026-09-03 21:27:50 +0900

    feat: #655 어드민 프리셋 등록/수정/삭제 REST API 및 SSR 대시보드 연동

--- houme-application/src/main/java/or/sopt/houme/compare/application/PresetUseCase.java
+++ houme-application/src/main/java/or/sopt/houme/compare/application/PresetUseCase.java
@@ -2,8 +2,12 @@ package or.sopt.houme.compare.application;
 
 import or.sopt.houme.compare.application.dto.PresetDetailResponse;
 import or.sopt.houme.compare.application.dto.PresetListResponse;
+import or.sopt.houme.compare.application.dto.SavePresetRequest;
 
 public interface PresetUseCase {
     PresetListResponse getPresets();
     PresetDetailResponse getPresetDetail(Long presetId);
+    Long createPreset(SavePresetRequest request);
+    void updatePreset(Long presetId, SavePresetRequest request);
+    void deletePreset(Long presetId);
 }
